cb8cdaacfd07778d7337b4e3079ea7e1f7f890c5
Henry Spindell <henryspindell@gmail.com>
2015-04-20 14:10:20 -0500

    updated validator to take game ID as arg, do deeper analysis

--- EventValidator/validator.py
+++ EventValidator/validator.py
@@ -1,9 +1,22 @@
+#!/usr/bin/python
+
 from parse_rest.connection import register
 from parse_rest.datatypes import Object
-from datetime import timedelta
+import datetime
+import sys
 
 def time_diff(e1, e2):
 	return abs(e1.createdAt - e2.createdAt)
+
+# TODO don't repeat this check a million times
+def different_people(e1,e2):
+	if hasattr(e1, 'submittedBy') and hasattr(e2, 'submittedBy'):
+		return e1.submittedBy != e2.submittedBy
+
+	if hasattr(e1, 'guestName') and hasattr(e2, 'guestName'):
+		return e1.guestName != e2.guestName
+
+	return True
 
 # connect to parse and initialize object types
 class Game(Object):
@@ -14,19 +27,36 @@ class Event(Object):
 
 
 def main():
+	if len(sys.argv) < 2:
+		print "No game specified"
+		return
 	register("k6BA02wILh0LWU04kWB1CT6HidrHkcqKSB15QxTv", "lg985CdYU3sIaeui5M5AST3WTmRaTVodB0wSJUTS", master_key="zKomCssbmWbmiPAFIU5zsul4bZajoeqgmy3ytRxW")
 
 	# get game and related events
-	game = Game.Query.get(objectId="yYhubNzW7F") #nIM6FMf7o5
-	events = Event.Query.filter(game=game).order_by("createdAt")
+	game = Game.Query.get(objectId=sys.argv[1])
+	events = Event.Query.all().filter(game=game).order_by("createdAt").limit(1000)
+	# TODO figure out a way to get ALL data, ignore limit
 
+	unique_users = {}
+	num_users = 0
+	print "\n{} total events recorded for {} @ {}:\n======================".format(len(events),game.teams[1], game.teams[0])
 	# track if an event has already been clustered so we don't have to look at it again
-	print "\nEvents recorded:\n======================"
 	for e1 in events:
-		print "{}@{}".format(e1.type,e1.createdAt)
-		e1.clustered = False
+		if hasattr(e1, 'submittedBy') and str(e1.submittedBy) not in unique_users:
+			unique_users[str(e1.submittedBy)] = e1.submittedBy
+			num_users += 1
+		elif hasattr(e1, 'guestName') and e1.guestName not in unique_users:
+			unique_users[e1.guestName] = e1.guestName
+			num_users += 1
 
+		print "{} @ {}".format(e1.type,e1.createdAt.strftime("%m-%d %H:%M:%S"))
+		e1.clustered = False
+	print "\n{} unique users recorded at least one event for this game.".format(num_users)
+
+
+	num_clustered = 0
 	valid_events = []
+	confidence_map ={}
 
 	# brute force n^2 version
 	# greedy chronologically
@@ -38,20 +68,34 @@ def main():
 			for e2 in events:
 				# and e1.submittedBy != e2.submittedBy
 				# removed that constraint for testing
-				if e1.objectId != e2.objectId and e1.type == e2.type and not e2.clustered:
-					if time_diff(e1,e2) <= timedelta(seconds=5.0):
+				if e1.objectId != e2.objectId and e1.type == e2.type and not e2.clustered and different_people(e1,e2):
+					if time_diff(e1,e2) <= datetime.timedelta(seconds=5.0):
 						cluster.append(e2)
 						e2.clustered = True
 
 			if len(cluster) > 1:
-				valid_events.append(cluster[0]) # mark the earliest as the ground truth
+				num_clustered += len(cluster)
+				if len(cluster) in confidence_map:
+					confidence_map[len(cluster)] += 1
+				else:
+					confidence_map[len(cluster)] = 1
+				valid_events.append((cluster[0], len(cluster))) # mark the earliest as the ground truth
 
+	game_start = datetime.datetime(2015, 4, 18, 23, 58, 0)
+	print "\nTossed out {} unconfirmed recordings.".format(len(events) - num_clustered)
+	print "\nFound {} confirmed events for {} @ {}.\n==========================".format(len(valid_events),game.teams[1], game.teams[0])
+	print "(Times relative to game start at {})\n".format(game_start)
 
-	print "\nValidated events:\n=========================="
 	for ve in valid_events:
-		print "{} @ {}: Verified {} at time {}".format(game.teams[1], game.teams[0], ve.type, ve.createdAt)
+		# TODO for links in video, subtract all times by 5s to make it easier to watch?
+		# TODO use cluster sizes as confidence measure
+		print "{} @ {} (recorded by {} users)".format(ve[0].type, str(ve[0].createdAt-game_start).split('.')[0], ve[1])
 
+	print "\nFrequency of cluster sizes for confirmed events:\n=========================="
+	for sz in confidence_map:
+		print "{} people : {} events".format(sz, confidence_map[sz])
 	print "\n"
+	
 
 if __name__ == "__main__":
 	main()
